b3ad3a54a5429a767455628919218519e1d7dda0
math218 <iker.marquez@colegiolakeside.edu.mx>
2024-12-09 15:38:39 -0600

    Create math.js

---
+++ math.js
@@ -0,0 +1,10 @@
+parametros = new URLSearchParams;
+enter = parametros.get("enter");
+if (enter == false) {
+  window.location.href = "index2.html";
+}
+else if(enter == true) {
+}
+else {
+  window.location.href = "index2.html";
+}
