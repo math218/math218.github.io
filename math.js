6c0b8c3fd5feeb2eb7f001e7948f9881006b817f
math218 <iker.marquez@colegiolakeside.edu.mx>
2024-12-10 08:48:47 -0600

    Update math.js

--- math.js
+++ math.js
@@ -1,6 +1,1 @@
-const parametros = new URLSearchParams(window.location.search);
-const enter = parametros.get("enter");
 
-if (enter == "false") { // Check if enter is NOT explicitly "true"
-  window.location.href = "index2.html";
-}
